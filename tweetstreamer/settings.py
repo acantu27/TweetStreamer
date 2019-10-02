70e8f86ce92c9dc61ba59f3a100b6ec980614f16
Alex Cantu <cantua43@gator.uhd.edu>
2019-10-02 15:40:14 -0500

    Minor Changes: Version 1.1 - Updated dependency due to potential security concern - Removed unnecessary dependencies - Switched output from sqlite to csv - Added additional features (setup mode, optional color output, terse output, additional arguments) - Optimized images/preview.gif

--- tweetstreamer/settings.py
+++ tweetstreamer/settings.py
@@ -1,8 +1,8 @@
 """Twitter API Credentials"""
-CONSUMER_KEY = 'tKMIBVQR8moUGsyoNx5i3oYWY'
-CONSUMER_SECRET = '6OIZdRix2f9ekERII7q44hoQL6jUKwrF9JGJ1kvWQIBUbZ68MA'
-ACCESS_TOKEN = '972327883065708544-TkTaaztHbXzsg5ds3XSngpBDf61FXoP'
-ACCESS_SECRET = 'Fl7OxaGXqaYhbPQ6zDWu3jIAJ1a2LXh3UImdA7nmWxaXZ'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
 
 """Terms used when filtering for specified tweets
 There is a limit on tweets that can be streamed"""
